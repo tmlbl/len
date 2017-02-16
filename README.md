7b5c08a5a033a4016dba7f6469cbed48bf1b63b4
Tim Lebel <tim@corporation.enterprises>
2017-02-16 01:08:32 -0800

    Add len code

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # len
-A minimal line length checker tool
+
+len is a command-line tool for checking if files contain lines longer than a
+specified maximum length. The file patterns to check and the line length are
+configurable. Provided that you have your go bin in your `PATH` the following
+will work:
+
+```
+go get github.com/tmlbl/len
+```
+
+Then run `len -h` for usage.
---
+++ len.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var (
+	glob   string
+	maxlen int
+	passed = true
+)
+
+type longline struct {
+	lineno int
+	text   string
+	fpath  string
+}
+
+func (l longline) Show() {
+	fmt.Fprintf(os.Stderr, "%s:%d line length is %d\n",
+		l.fpath, l.lineno, len(l.text))
+}
+
+func exitIfErr(err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func main() {
+	flag.StringVar(&glob, "g", "*.go,**/*.go",
+		"File globs to test, comma-separated")
+	flag.IntVar(&maxlen, "len", 80, "Maximum line length to allow")
+	flag.Parse()
+
+	var files []string
+
+	for _, g := range strings.Split(glob, ",") {
+		f, err := filepath.Glob(g)
+		exitIfErr(err)
+		files = append(files, f...)
+	}
+
+	for _, fpath := range files {
+		file, err := os.Open(fpath)
+		exitIfErr(err)
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		var lineno int
+		for scanner.Scan() {
+			lineno++
+			ln := scanner.Text()
+			if len(ln) > maxlen {
+				ll := longline{lineno, ln, fpath}
+				ll.Show()
+				passed = false
+			}
+		}
+		exitIfErr(scanner.Err())
+	}
+
+	if !passed {
+		exitIfErr(errors.New("line length check failed"))
+	}
+}
